739e07afd7f5eba8895e6b2eb56d926afe21c327
Rohan <rohansethi1999430@gmail.com>
2021-05-23 11:36:34 +0530

    adding animation when clicking roll dice

--- src/Die.js
+++ src/Die.js
@@ -4,7 +4,7 @@ class Die extends Component{
     render(){
         return(
             
-            <i class={` Die fas fa-dice-${this.props.face}`}></i>
+            <i class={` Die fas fa-dice-${this.props.face} ${this.props.rolling ? "shaking":""}`}></i>
 
         )
     }
--- src/RollDice.js
+++ src/RollDice.js
@@ -19,17 +19,23 @@ import './RollDice.css'
             this.props.sides[
              Math.floor(Math.random()*this.props.sides.length)
             ];
-            this.setState({die1:newDie1,die2:newDie2});
+            this.setState({die1:newDie1,die2:newDie2,rolling:true,});
+
+            setTimeout(()=>{
+                this.setState({rolling:false})
+            },1000);
      }
      render(){
          
          return(
              <div className="RollDice">
             <div className="RollDice-container">
-            <Die face={this.state.die1}/>
-                 <Die face={this.state.die2}/>
+            <Die face={this.state.die1}rolling={this.state.rolling}/>
+                 <Die face={this.state.die2}rolling={this.state.rolling}/>
             </div>
-                 <button onClick={this.roll}>Roll Dice!</button>
+                 <button onClick={this.roll}disabled={this.state.rolling}>
+                     {this.state.rolling ? 'Rolling...':'Roll Dice!'}
+                 </button>
              </div>
          )
      }
