784083823bec13e268dc4cdcf3ebf17c14bc474b
Rohan <rohansethi1999430@gmail.com>
2021-05-23 10:57:15 +0530

    adding css to RollDice Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import Die from './Die';
+
 import RollDice from './RollDice';
 
 function App() {
--- src/RollDice.js
+++ src/RollDice.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Die from './Die'
+import './RollDice.css'
  class RollDice extends Component{
      static defaultProps={
          sides:["one","two","three","four","five","six"]
@@ -23,9 +24,11 @@ import Die from './Die'
      render(){
          
          return(
-             <div>
-                 <Die face={this.state.die1}/>
+             <div className="RollDice">
+            <div className="RollDice-container">
+            <Die face={this.state.die1}/>
                  <Die face={this.state.die2}/>
+            </div>
                  <button onClick={this.roll}>Roll Dice!</button>
              </div>
          )
